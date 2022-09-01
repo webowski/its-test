597cf83f3a9cc90b0c1ac01a3d309c7b7eebb0b3
Aleksandr Osokin <osokinweb@gmail.com>
2022-09-01 17:41:56 +0300

    Adjust scroll disabling

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -20,16 +20,12 @@ export const useScroll = () => {
 		document.querySelector('body').addEventListener('wheel', preventScroll, {passive: false})
 		document.querySelector('body').addEventListener('touchmove', preventTouch, {passive: false})
 		document.querySelector('*:not(body)').addEventListener('touchmove', preventTouchPropagation, {passive: false})
-		document.querySelector('body').classList.add('G-disableScroll')
-		document.querySelector('html').classList.add('G-disableScroll')
 	}
 
 	const enableScroll = () => {
 		document.querySelector('body').removeEventListener('wheel', preventScroll)
 		document.querySelector('body').removeEventListener('touchmove', preventTouch)
 		document.querySelector('*:not(body)').removeEventListener('touchmove', preventTouchPropagation)
-		document.querySelector('body').classList.remove('G-disableScroll')
-		document.querySelector('html').classList.remove('G-disableScroll')
 	}
 
 	return {
