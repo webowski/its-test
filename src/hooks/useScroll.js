50f2558cad2da1355a0a37b8fe41a80dc323e5f6
Aleksandr Osokin <osokinweb@gmail.com>
2022-09-01 15:42:24 +0300

    Fix scroll disabling and button press

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -18,16 +18,16 @@ export const useScroll = () => {
 
 	const disableScroll = () => {
 		document.querySelector('body').addEventListener('wheel', preventScroll, {passive: false})
-		document.querySelector('body').addEventListener('touchstart', preventTouch, {passive: false})
-		document.querySelector('*:not(body)').addEventListener('touchstart', preventTouchPropagation, {passive: false})
+		document.querySelector('body').addEventListener('touchmove', preventTouch, {passive: false})
+		document.querySelector('*:not(body)').addEventListener('touchmove', preventTouchPropagation, {passive: false})
 		document.querySelector('body').classList.add('G-disableScroll')
 		document.querySelector('html').classList.add('G-disableScroll')
 	}
 
 	const enableScroll = () => {
-		document.querySelector('body').removeEventListener('wheel', preventScroll, {passive: false})
-		document.querySelector('body').removeEventListener('touchstart', preventTouch, {passive: false})
-		document.querySelector('*:not(body)').removeEventListener('touchstart', preventTouchPropagation, {passive: false})
+		document.querySelector('body').removeEventListener('wheel', preventScroll)
+		document.querySelector('body').removeEventListener('touchmove', preventTouch)
+		document.querySelector('*:not(body)').removeEventListener('touchmove', preventTouchPropagation)
 		document.querySelector('body').classList.remove('G-disableScroll')
 		document.querySelector('html').classList.remove('G-disableScroll')
 	}
