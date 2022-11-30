a696abfd899c4642f2c440f45aa43f45fdcddf45
Aleksandr Osokin <osokinweb@gmail.com>
2022-11-30 18:27:06 +0300

    Disable scroll preventing

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,24 +1,23 @@
 import { ref } from 'vue'
 
 export const useScroll = () => {
-
 	const top = ref(0)
 
-	const preventScroll = (e) => {
-		e.preventDefault()
-		e.stopPropagation()
-		return false
-	}
+	// const preventScroll = (e) => {
+	// 	e.preventDefault()
+	// 	e.stopPropagation()
+	// 	return false
+	// }
 
 	const disableScroll = () => {
-		document.body.addEventListener('wheel', preventScroll, {passive: false})
+		// document.body.addEventListener('wheel', preventScroll, {passive: false})
 		top.value = document.documentElement.scrollTop
 		document.body.classList.add('G-disableScroll')
 		document.body.style.top = '-' + top.value + 'px'
 	}
 
 	const enableScroll = () => {
-		document.body.removeEventListener('wheel', preventScroll)
+		// document.body.removeEventListener('wheel', preventScroll)
 		document.body.classList.remove('G-disableScroll')
 		document.documentElement.scrollTop = document.body.scrollTop = top.value
 		document.body.style.top = null
@@ -26,6 +25,6 @@ export const useScroll = () => {
 
 	return {
 		disableScroll,
-		enableScroll
+		enableScroll,
 	}
 }
